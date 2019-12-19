b1d81766791aedc6b6618b86c3c38f4204bff07e
sahilpkolkata <36637736+sahilpkolkata@users.noreply.github.com>
2019-12-19 23:32:04 +0530

    deleted dev.js

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    DB_URL: "mongodb://localhost/innotel",
-    SECRET: "mYhOtel1234",
-    MONGODB_URL: "mongodb+srv://avengers:endgame@mycluster1-sz7hq.mongodb.net/test?retryWrites=true&w=majority"
-}
