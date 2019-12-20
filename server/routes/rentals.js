248cde89b81dc6fb376f3369eec69151f46f867e
sahilpkolkata <sahilpkolkata@gmail.com>
2019-12-20 20:47:15 +0530

    Added search and create rental functionality

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const Rental = require('../models/rental')
+const User = require('../models/user')
+const { normalizeErrors } = require('../helpers/mongoose')
+const UserCtrl = require('../controllers/user')
 
-router.get('',function(req,res){
-    Rental.find({})
-        .select('-bookings')
-        .exec(
-            function(err,foundRentals){
-        
-                res.json(foundRentals)
-            })
-})
+
  
 router.get('/:id',function(req,res){
     const rentalId = req.params.id;
@@ -26,4 +21,40 @@ router.get('/:id',function(req,res){
             return res.json(foundRental)
         })
 })
+
+router.post('',UserCtrl.authMiddleware,function(req,res){
+    const { title, city, street, category, image, bedrooms, shared, description, dailyRate} = req.body
+    const user = res.locals.user
+
+    const rental = new Rental({title, city, street, category, image, bedrooms, shared, description, dailyRate})
+    rental.user = user
+    Rental.create(rental, function(err, newRental){
+        if(err){
+            return res.status(422).send({errors:normalizeErrors(err.errors)})
+        }
+
+        User.update({_id: user.id},{ $push:{rentals:newRental}}, function(){})
+        return res.json(newRental)
+    })
+})
+
+router.get('',function(req,res){
+    const city = req.query.city
+    const query = city ? {city:city.toLowerCase()} : {}
+
+        Rental.find(query)
+        .select('-bookings')
+        .exec(
+            function(err,foundRentals){
+                if(err){
+                    return res.status(422).send({errors:normalizeErrors(err.errors)})
+                  }
+                  if(city && foundRentals === 0){
+                    return res.status(422).send({errors:[{title:"No Rental Found", detail:`There are no rental for city ${city}`}]})
+                  }
+                 return res.json(foundRentals)
+            })
+})
+
+
 module.exports = router
